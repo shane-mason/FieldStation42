4d359186d98c7c3daca8d50455e4ee051c6fd7de
Shane C Mason <shane.c.mason@gmail.com>
2024-08-15 18:02:29 -0700

    Update aerial_listener.py

--- aerial_listener.py
+++ aerial_listener.py
@@ -6,17 +6,27 @@ import datetime
 # listens for input on 'button press' and then writes to channel socket file (player will pick it up.)
 # uses circuit python
 
-button = digitalio.DigitalInOut(board.D4)
-button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.UP
+#button = digitalio.DigitalInOut(board.D4)
+#button.direction = digitalio.Direction.INPUT
+#button.pull = digitalio.Pull.UP
 
+#while True:
+#    if not button.value:
+#        timestamp = datetime.datetime.now
+#        with open("runtime/channel.socket") as fp:
+#            fp.write(timestamp)
+
+#        time.sleep(5)
+#    else:
+#        time.sleep(.05)
+
+
+
+#uart = busio.UART(tx=board.GP0, rx=board.GP1)
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=10)
 while True:
-    if not button.value:
-        timestamp = datetime.datetime.now
-        with open("runtime/channel.socket") as fp:
-            fp.write(timestamp)
-
-        time.sleep(5)
-    else:
-        time.sleep(.05)
-
+    time.sleep(0.01)
+    if (uart.any() > 0):
+        message = uart.read()
+        command = message.decode('ascii')
+        print("Got Message: ", command)
