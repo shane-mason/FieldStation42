2b856392f576f78aa6912b3674378e66769c4598
Brent <InternalCorporation@proton.me>
2024-09-28 11:51:35 -0400

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,53 @@
+git clone 
+sudo mkdir catalog
+cd catalog
+sudo mkdir abc_catalog
+cd abc_catalog
+sudo mkdir bumps
+sudo mkdir cartoon
+sudo mkdir commercials
+sudo mkdir daytime
+sudo mkdir late
+sudo mkdir morning
+sudo mkdir primetime
+sudo mkdir sports
+sudo mkdir bumps
+cd ..
+sudo mkdir cbs_catalog
+cd abc_catalog
+sudo mkdir bumps
+sudo mkdir cartoon
+sudo mkdir commercials
+sudo mkdir daytime
+sudo mkdir late
+sudo mkdir morning
+sudo mkdir primetime
+sudo mkdir sports
+sudo mkdir bumps
+cd ..
+sudo mkdir nbc_catalog
+cd abc_catalog
+sudo mkdir bumps
+sudo mkdir cartoon
+sudo mkdir commercials
+sudo mkdir daytime
+sudo mkdir late
+sudo mkdir morning
+sudo mkdir primetime
+sudo mkdir sports
+sudo mkdir bumps
+cd ..
+sudo mkdir pbs_catalog
+cd abc_catalog
+sudo mkdir bumps
+sudo mkdir cartoon
+sudo mkdir commercials
+sudo mkdir daytime
+sudo mkdir late
+sudo mkdir morning
+sudo mkdir primetime
+sudo mkdir sports
+sudo mkdir bumps
+cd ..
+echo "Add your videos in the folders in the Catalog folders directory. The catalog folders have The station name then _catalog for example pbs_catalog"
+echo "Continuing install."
