08a1285dbe1f123d3e848ca92c1fd7a7713bb3f4
Brent <InternalCorporation@proton.me>
2024-09-27 20:04:42 -0400

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,12 @@
+echo "Welcome to the auto install. Please stand by."
+echo "Updating and upgrading packages to not get errors."
+sudo apt update && sudo apt upgrade
+echo "Upgrade finished with no errors."
+echo "Installing Deps"
+echo "Installing Git"
+sudo apt install git
+echo "Installing python"
+sudo apt install python
+echo "Installing mpv"
+sudo apt install mpv
+end
