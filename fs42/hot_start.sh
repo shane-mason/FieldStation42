806e871cde8a292b003810cc81a03530fdcde520
Shane C Mason <shane.c.mason@gmail.com>
2024-10-13 11:27:50 -0700

    Update path

--- fs42/hot_start.sh
+++ fs42/hot_start.sh
@@ -4,4 +4,4 @@ cd ~/FieldStation42
 . env/bin/activate
 
 python3 field_player.py 1>/dev/null 2>/dev/null & disown
-python3 command_input.py 1>/dev/null 2>/dev/null & disown
+python3 fs42/command_input.py 1>/dev/null 2>/dev/null & disown
