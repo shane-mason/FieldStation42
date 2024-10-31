b5f521fb61e720a10845b93429ac521294f685ef
Devineavenger <72168455+Devineavenger@users.noreply.github.com>
2024-10-31 09:18:22 +0200

    moved .py to fs42 and update .sh

--- rebuild_catalog.sh
+++ rebuild_catalog.sh
@@ -4,4 +4,4 @@
 cd $(dirname $0)
 . env/bin/activate
 
-python3 station_42.py --rebuild_catalog
+python3 fs42/station_42.py --rebuild_catalog
--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@
 cd $(dirname $0)
 . env/bin/activate
 
-python3 field_player.py
+python3 fs42/field_player.py
