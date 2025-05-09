91ff34fbc8e9833e54ca54962b0aa6ed43d38648
matthew-c-lee <mail@matthewlee.name>
2025-05-09 00:35:08 -0500

    Switch to pytest

--- test/test_catalog.py
+++ test/test_catalog.py
@@ -1,36 +1,32 @@
 from fs42.catalog import ShowCatalog
 from fs42.schedule_hint import MonthHint, QuarterHint, RangeHint
-import unittest
 
 TEST_CONF = {"network_name": "TEST"}
 
-class TestCatalogHints(unittest.TestCase):
+def test_process_subdir_not():
+    hints = ShowCatalog._process_hints("should be none")
+    assert len(hints) == 0
 
-    def test_process_subdir_not(self):
-        hints = ShowCatalog._process_hints("should be none")
-        self.assertEqual(len(hints), 0)
+def test_process_subdir_month():
+    hints = ShowCatalog._process_hints("March")
+    assert len(hints) == 1
+    assert isinstance(hints[0], MonthHint)
 
-    def test_process_subdir_month(self):
-        hints = ShowCatalog._process_hints("March")
-        self.assertEqual(len(hints), 1)
-        self.assertIsInstance(hints[0], MonthHint)
+def test_process_subdir_quarter():
+    hints = ShowCatalog._process_hints("q1")
+    assert len(hints) == 1
+    assert isinstance(hints[0], QuarterHint)
 
-    def test_process_subdir_quarter(self):
-        hints = ShowCatalog._process_hints("q1")
-        self.assertEqual(len(hints), 1)
-        self.assertIsInstance(hints[0], QuarterHint)
+def test_process_subdir_range():
+    hints = ShowCatalog._process_hints("March 1 - March 15")
+    assert len(hints) == 1
+    assert isinstance(hints[0], RangeHint)
 
-    def test_process_subdir_range(self):
-        hints = ShowCatalog._process_hints("March 1 - March 15")
-        self.assertEqual(len(hints), 1)
-        self.assertIsInstance(hints[0], RangeHint)
+def test_process_subdir_path():
+    hints = ShowCatalog._process_hints("somepath/somedir/March 1 - March 15")
+    assert len(hints) == 1
+    assert isinstance(hints[0], RangeHint)
 
-    def test_process_subdir_path(self):
-        hints = ShowCatalog._process_hints("somepath/somedir/March 1 - March 15")
-        self.assertEqual(len(hints), 1)
-        self.assertIsInstance(hints[0], RangeHint)
-        hints = ShowCatalog._process_hints("March/somedir/March 1 - March 15")
-        self.assertEqual(len(hints), 1)
-        self.assertIsInstance(hints[0], RangeHint)
-
-
+    hints = ShowCatalog._process_hints("March/somedir/March 1 - March 15")
+    assert len(hints) == 1
+    assert isinstance(hints[0], RangeHint)
