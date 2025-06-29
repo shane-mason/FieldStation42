fe0e2836af350ea88f6bde3264c70a0f1aa35336
Shane C Mason <shane.c.mason@gmail.com>
2025-06-28 22:41:58 -0700

    add webm files

--- fs42/media_processor.py
+++ fs42/media_processor.py
@@ -17,7 +17,7 @@ from fs42.catalog_entry import CatalogEntry
 
 
 class MediaProcessor:
-    supported_formats = ["mp4", "mpg", "mpeg", "avi", "mov", "mkv", "ts", "m4v"]
+    supported_formats = ["mp4", "mpg", "mpeg", "avi", "mov", "mkv", "ts", "m4v", "webm", "wmv"]
 
     def process_one(fname, tag, hints, fluid=None) -> CatalogEntry:
         _l = logging.getLogger("MEDIA")
