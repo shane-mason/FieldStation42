8fe719b9eb8841f4be40b515765e6764e873673a
Shane C Mason <shane.c.mason@gmail.com>
2025-07-03 07:13:10 -0700

    Update catalog_api.py

--- fs42/catalog_api.py
+++ fs42/catalog_api.py
@@ -1,8 +1,8 @@
 from fs42.catalog_io import CatalogIO
 from fs42.catalog_entry import CatalogEntry
 
-class CatalogAPI:
 
+class CatalogAPI:
     @staticmethod
     def delete_catalog(station_config):
         CatalogIO().delete_all_entries_for_station(station_config["network_name"])
@@ -10,24 +10,20 @@ class CatalogAPI:
     @staticmethod
     def set_entries(station_config, entries: list[CatalogEntry]):
         CatalogAPI.delete_catalog(station_config)
-        CatalogIO().put_catalog_entries(
-            station_config["network_name"],
-            entries
-        )
-    
+        CatalogIO().put_catalog_entries(station_config["network_name"], entries)
 
     @staticmethod
     def get_entries(station_config):
         return CatalogIO().get_catalog_entries(station_config["network_name"])
-    
+
     @staticmethod
     def get_by_tag(station_config, tag):
         return CatalogIO().get_by_tag(station_config["network_name"], tag)
-    
+
     @staticmethod
     def get_by_path(station_config, path):
         return CatalogIO().get_entry_by_path(station_config["network_name"], path)
- 
+
     @staticmethod
     def update_play_counts(station_config, entries: list[CatalogEntry]):
         # flatten the entries list
@@ -37,7 +33,4 @@ class CatalogAPI:
                 flat.extend(entry)
             else:
                 flat.append(entry)
-        CatalogIO().batch_increment_counts(
-            station_config["network_name"],
-            flat
-        )
+        CatalogIO().batch_increment_counts(station_config["network_name"], flat)
