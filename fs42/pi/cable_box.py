c67c3de53329fd298e7373a438f986be8a77355c
Shane C Mason <shane.c.mason@gmail.com>
2025-07-20 08:48:41 -0700

    remove bare exception

--- fs42/pi/cable_box.py
+++ fs42/pi/cable_box.py
@@ -126,7 +126,7 @@ class CableBox:
                         as_num = int(last_pressed + key_pressed)
                         last_pressed = key_pressed
                         self.tm.show(f"  {as_num:02d}")
-                    except:
+                    except Exception:
                         pass
 
                 time.sleep(0.3)
