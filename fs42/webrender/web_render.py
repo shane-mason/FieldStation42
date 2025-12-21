64e6b6af3cb0cadb5600a667b1e7335a1414d382
Shane C Mason <shane.c.mason@gmail.com>
2025-12-21 10:44:06 -0800

    testing positioning

--- fs42/webrender/web_render.py
+++ fs42/webrender/web_render.py
@@ -176,8 +176,20 @@ if __name__ == "__main__":
     import sys
     import os
     sys.path.append(os.getcwd())
-    from fs42.station_manager import StationManager
-    conf = StationManager().station_by_name("NBC")
-    web_render_runner(conf, None)
-    
-    #https://mwood77.github.io/ws4kp-international/?hazards-checkbox=false&current-weather-checkbox=true&latest-observations-checkbox=false&hourly-checkbox=true&hourly-graph-checkbox=true&travel-checkbox=false&regional-forecast-checkbox=true&local-forecast-checkbox=true&extended-forecast-checkbox=true&almanac-checkbox=true&radar-checkbox=true&marine-forecast-checkbox=true&aqi-forecast-checkbox=true&settings-experimentalFeatures-checkbox=false&settings-hideWebamp-checkbox=false&settings-kiosk-checkbox=true&settings-scanLines-checkbox=false&settings-wide-checkbox=false&chkAutoRefresh=true&settings-windUnits-select=4.00&settings-marineWindUnits-select=1.00&settings-marineWaveHeightUnits-select=1.00&settings-temperatureUnits-select=2.00&settings-distanceUnits-select=2.00&settings-pressureUnits-select=1.00&settings-hoursFormat-select=1.00&settings-speed-select=1.00&latLonQuery=Seattle%2C+WA%2C+USA&latLon=%7B%22lat%22%3A47.6032%2C%22lon%22%3A-122.3302%7D
+
+    # Simple test config - customize as needed
+    test_conf = {
+        "url": "http://localhost:4242/static/diagnostics.html",
+        "width": 800,
+        "height": 600,
+        # Uncomment to test specific positioning:
+        # "x": 100,
+        # "y": 100,
+    }
+
+    # Or load a station config by name
+    # Uncomment and modify to use a station config:
+    # from fs42.station_manager import StationManager
+    # test_conf = StationManager().station_by_name("diagnostic")
+
+    web_render_runner(test_conf, None)
