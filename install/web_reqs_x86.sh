66a754944ce2cdea13e2e30e48fd96aa2c41cc5c
Estacaco <44181620+Estacaco@users.noreply.github.com>
2026-04-11 16:28:59 -0500

    Create web_reqs_x86.sh (#554)
    Add Web Dependencies script for x86

---
+++ install/web_reqs_x86.sh
@@ -0,0 +1,10 @@
+
+apt-get install libwebp7
+ln -s /usr/lib/x86_64-linux-gnu/libwebp.so.7 /usr/lib/x86_64-linux-gnu/libwebp.so.6
+
+apt-get install minizip
+
+apt-get install libtiff5-dev
+ln -s /usr/lib/x86_64-linux-gnu/libtiff.so.6 /usr/lib/x86_64-linux-gnu/libtiff.so.5
+
+apt-get install libxcb-cursor0
