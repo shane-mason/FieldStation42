67956dfaab4e573c1a78afa0f5ffeed053cd3b9f
Moe Fwacky <moe.fwacky@gmail.com>
2026-08-31 20:46:35 -0700

    Added support for a json file for keymapping (#653)
    * Added support for a json file for keymapping
    
    Changed keymap behavior to look first for keymap.json in the same directory, if not found falls back to defaults.
    keymap.json file uses the same format as `KEY_MAPPINGS`
    
    * changed keymap file lookup location
    
    Modified the keymap file lookup location to be in-line with similar lookups in the runtime directory
    
    * Made modifications to allow other configurations via file
    
    The script now looks for DEBOUNCE_TIME, USE_SYSTEMCTL and SYSTEMCTL_TO_TOGGLE in the json file, as well as the keymap, allowing these to be set outside of the script. Anything not found in the external file falls back to the defaults set in the script.
    
    * Added error catching and dictionary merge
    
    Added error handling to code that loads settings from file
    Added dictionary merge to keep keymap defaults if not present in the file
    
    * Fixed a remap error
    
    Added a check to ensure a key that is mapped to a function doesn't also retain its default function mapping.

--- fs42/pi/remote_controller.py
+++ fs42/pi/remote_controller.py
@@ -36,7 +36,7 @@ USE_SYSTEMCTL = True
 # Available key names: 'home', 'end', 'up', 'down', 'left', 'right', 'space', 'enter',
 # 'esc', 'tab', 'backspace', 'delete', 'insert', 'pageup', 'pagedown', 'f1'-'f12',
 # 'a'-'z', 'leftshift', 'rightshift', 'leftctrl', 'rightctrl', 'leftalt', 'rightalt'
-KEY_MAPPINGS = {
+DEFAULT_KEY_MAPPINGS = {
     # Remote control functions
     'show_guide': 'home',        # Show program guide
     'volume_up': 'right',        # Increase volume
@@ -57,6 +57,30 @@ KEY_MAPPINGS = {
     # 'volume_up': 'pageup',     # Use page up for volume up
     # 'volume_down': 'pagedown', # Use page down for volume down
 }
+if os.path.exists('runtime/remote_controller.json'):
+    print("Loading settings from runtime/remote_controller.json")
+    try:
+        with open('runtime/remote_controller.json','r') as file:
+            remote_settings = json.load(file)
+        if remote_settings.get("use_systemctl") is not None:
+            print("Loading USE_SYSTEMCTL from file")
+            if remote_settings.get("use_systemctl") is False:
+                USE_SYSTEMCTL = False
+        if remote_settings.get("systemctl_to_toggle") is not None:
+            print("Loading SYSTEMCTL_TO_TOGGLE from file")
+            SYSTEMCTL_TO_TOGGLE = remote_settings.get("systemctl_to_toggle")
+        if remote_settings.get("key_mappings") is not None:
+            print("Loading KEY_MAPPINGS from file")
+            file_mappings = remote_settings["key_mappings"]
+            KEY_MAPPINGS = file_mappings.copy()
+            for function, key in DEFAULT_KEY_MAPPINGS.items():
+                if function not in KEY_MAPPINGS and key not in KEY_MAPPINGS.values():
+                    KEY_MAPPINGS[function] = key
+    except:
+        print("Error loading settings from file")
+else:
+    KEY_MAPPINGS = DEFAULT_KEY_MAPPINGS.copy()
+    
 CALLBACK_MAP_FILE = "runtime/remote_callback_map.json"
 PRESS_SOCKET = "runtime/press.socket"
 
